9c48d86e950a9bd79ee30b6dceb0b3a0180ef46c
frostzzone <65735427+frostzzone@users.noreply.github.com>
2025-04-17 19:35:53 -0700

    Revival
    Fixed psych charts and maybe maintain this for a bit

--- README.md
+++ README.md
@@ -1,11 +1,35 @@
 # [fnf-song-converter](https://cabalex.github.io/fnf-song-converter/)
 
-## ⚠️ Archive notice
+## TOC (table of contents)
 
-I no longer have the capability to respond to issues for this converter, so I've archived it until further notice. You're welcome to fork it and make your own changes.
+- [Info](#-info)
+- [Original Readme](#-original-readme)
 
-Scratch on!
+## 📜 Info
 
-A song converter for [my Scratch FNF clone](https://scratch.mit.edu/projects/519325355/). The converter converts song JSON files made in the [Friday Night Funkin'](https://ninja-muffin24.itch.io/funkin) song editor (go into a song and press 7) to TXT files that can be imported into Scratch lists (right click on the list > import...).
+As the original converter hasnt been updated in a long time, and has been archived, i have taken it upon myself to fix some bugs and maybe add .fnfc support
 
-Coded with [pretty bad] HTML and JS.
+This is a song converter for [cabalex's Scratch FNF clone](https://scratch.mit.edu/projects/519325355/).
+
+The converter converts song JSON files made in the [Friday Night Funkin'](https://ninja-muffin24.itch.io/funkin) or [Psych Engine](https://github.com/ShadowMario/FNF-PsychEngine) song editor
+
+And as cabalex has stated, this really is Coded with [pretty bad] HTML and JS.
+
+## 📃 To-do
+
+- ~~Fix psych charts~~
+  - Was caused by the removal of the initial song object
+- Add fnfc Support
+
+- Maybe more soon if someone makes a suggestion
+
+## 😢 Original Readme
+> ## ⚠️ Archive notice
+>
+> I no longer have the capability to respond to issues for this converter, so I've archived it until further notice. You're welcome to fork it and make your own changes.
+>
+> Scratch on!
+>
+> A song converter for [my Scratch FNF clone](https://scratch.mit.edu/projects/519325355/). The converter converts song JSON files made in the [Friday Night Funkin'](https://ninja-muffin24.itch.io/funkin) song editor (go into a song and press 7) to TXT files that can be imported into Scratch lists (right click on the list > import...).
+>
+>Coded with [pretty bad] HTML and JS.
--- convert.js
+++ convert.js
@@ -12,64 +12,72 @@ function dragOverHandler(ev) {
 }
 
 function dropHandler(ev) {
-    // Prevent default behavior (Prevent file from being opened)
-    ev.preventDefault();
-    var files = [];
-    if (ev.dataTransfer.items) {
-        // Use DataTransferItemList interface to access the file(s)
-        for (var i = 0; i < ev.dataTransfer.items.length; i++) {
-            // If dropped items aren't files, reject them
-            if (ev.dataTransfer.items[i].kind === 'file') {
-                files.push(ev.dataTransfer.items[i].getAsFile());
-            };
-        };
-    } else {
-        // Use DataTransfer interface to access the file(s)
-        for (var i = 0; i < ev.dataTransfer.files.length; i++) {
-        files.push(ev.dataTransfer.files[i]);
-        };
-    };
-    if (files.length > 0) {
-        loadFile(files[0]);
-    };
+  // Prevent default behavior (Prevent file from being opened)
+  ev.preventDefault();
+  var files = [];
+  if (ev.dataTransfer.items) {
+    // Use DataTransferItemList interface to access the file(s)
+    for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+      // If dropped items aren't files, reject them
+      if (ev.dataTransfer.items[i].kind === "file") {
+        files.push(ev.dataTransfer.items[i].getAsFile());
+      }
+    }
+  } else {
+    // Use DataTransfer interface to access the file(s)
+    for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+      files.push(ev.dataTransfer.files[i]);
+    }
+  }
+  if (files.length > 0) {
+    loadFile(files[0]);
+  }
 }
 
 function download() {
-    const link = document.createElement('a');
-    link.href = "https://github.com/cabalex/fnf-song-converter/wiki/Making-and-Importing-New-Songs#step-3--importing-your-song-map"
-    link.text = "Not sure where to go next? Click here!"
-    outputArea.appendChild(link);
+  const link = document.createElement("a");
+  link.href =
+    "https://github.com/cabalex/fnf-song-converter/wiki/Making-and-Importing-New-Songs#step-3--importing-your-song-map";
+  link.text = "Not sure where to go next? Click here!";
+  outputArea.appendChild(link);
 }
 
 inputElement.onchange = (e) => {
   loadFile(inputElement.files[0]);
-}
+};
 
 function loadFile(file) {
   if (!file) return;
 
-  if (!file.name.endsWith('.json')) {
-    alert("Enter a JSON file, silly!\nClick 'Save' in the beatmap editor and use that file.");
-    return
+  if (!file.name.endsWith(".json")) {
+    alert(
+      "Enter a JSON file, silly!\nClick 'Save' in the beatmap editor and use that file."
+    );
+    return;
   }
-  
+
   fileNameArea.innerHTML = file.name;
   let reader = new FileReader();
 
   reader.onload = (e) => {
     // Handle dropdown - "modulo", "ignore", "expanded"
-    noteHandlingOption = document.getElementById("noteHandlingSelector").value
+    noteHandlingOption = document.getElementById("noteHandlingSelector").value;
 
     const json = JSON.parse(
-        e.target.result.replace(/^\0+/, '').replace(/\0+$/, '') // sometimes has \x00 at the end
+      e.target.result.replace(/^\0+/, "").replace(/\0+$/, "") // sometimes has \x00 at the end
     );
 
+    // Check if song is an object (fixes newer psych chart file format)
+    if (typeof json.song === "object") json = json.song;
+
     // display bpm and speed, reset output area
-    var outputString = '';
-    outputArea.innerHTML = '';
+    var outputString = "";
+    outputArea.innerHTML = "";
     const outputTextInitial = document.createElement("h3");
-    outputTextInitial.style = "text-align: center; display: block;"
-    outputTextInitial.innerHTML = `${json.song.song} | ${json.song.bpm} BPM | SPEED ${parseFloat(json.song.speed).toFixed(2)}`
+    outputTextInitial.style = "text-align: center; display: block;";
+    outputTextInitial.innerHTML = `${json.song.song} | ${
+      json.song.bpm
+    } BPM | SPEED ${parseFloat(json.song.speed).toFixed(2)}`;
     outputArea.appendChild(outputTextInitial);
 
     const bpm = json.song.bpm;
@@ -79,120 +87,142 @@ function loadFile(file) {
     var beginsection_timing = 0;
 
     function addToNotesList(timing, value) {
-        if (!notesList[timing]) {
-            notesList[timing] = [value];
-        } else {
-            notesList[timing].push(value);
-        }
+      if (!notesList[timing]) {
+        notesList[timing] = [value];
+      } else {
+        notesList[timing].push(value);
+      }
     }
 
     for (i = 0; i < json.song.notes.length; i++) {
-        // sections
-        const section = json.song.notes[i];
+      // sections
+      const section = json.song.notes[i];
 
-        if (section.mustHitSection == false) {
-            var assignment = [0, 1, 2, 3, 4, 5, 6, 7];
-            sectionList[beginsection_timing.toString()] = "0" 
-        } else {
-            var assignment = [4, 5, 6, 7, 0, 1, 2, 3];
-            sectionList[beginsection_timing.toString()] = "1"
+      if (section.mustHitSection == false) {
+        var assignment = [0, 1, 2, 3, 4, 5, 6, 7];
+        sectionList[beginsection_timing.toString()] = "0";
+      } else {
+        var assignment = [4, 5, 6, 7, 0, 1, 2, 3];
+        sectionList[beginsection_timing.toString()] = "1";
+      }
+
+      beginsection_timing +=
+        (((60 / bpm) * 4) / 16) * section.lengthInSteps * 1000;
+
+      // notes
+      for (x = 0; x < section.sectionNotes.length; x++) {
+        let note = section.sectionNotes[x];
+        let timing = note[0].toFixed(4).padStart(12, "0");
+
+        if (note[0].toString().split(".").length == 1) {
+          timing = note[0].toString().padStart(7, "0");
         }
 
-        beginsection_timing += ((60 / bpm) * 4) / 16 * section.lengthInSteps * 1000;
-
-        // notes
-        for (x = 0; x < section.sectionNotes.length; x++) {
-            let note = section.sectionNotes[x];
-            let timing = note[0].toFixed(4).padStart(12, '0')
-
-            if (note[0].toString().split(".").length == 1) {
-                timing = note[0].toString().padStart(7, '0')
+        switch (noteHandlingOption) {
+          case "modulo":
+            // truncates modified notes (e.g. hurt notes)
+            if (note.length == 3) {
+              addToNotesList(timing, `${assignment[note[1] % 8]}_${note[2]}`);
             }
-
-            switch(noteHandlingOption) {
-                case "modulo":
-                    // truncates modified notes (e.g. hurt notes)
-                    if (note.length == 3) {
-                        addToNotesList(timing, `${assignment[note[1]%8]}_${note[2]}`);
-                    }
-                    break;
-                case "ignore":
-                    // note modifiers are often index 3 (length 4)
-                    if (note[1] < 8 && note.length == 3) {
-                        addToNotesList(timing, `${assignment[note[1]]}_${note[2]}`);
-                    }
-                    break;
-                case "expanded_truncate":
-                    addToNotesList(timing, `${Math.floor(note[1]/8)*8 + assignment[note[1]%8]}_${note[2]}`);
-                    break;
-                case "expanded_unmodified_truncate":
-                    if (note[1] < 8) {
-                        addToNotesList(timing, `${assignment[note[1]]}_${note[2]}`);
-                    } else {
-                        addToNotesList(timing, `${note[1]}_${note[2]}`);
-                    }
-                    break;
-                case "expanded":
-                    addToNotesList(timing, `${Math.floor(note[1]/8)*8 + assignment[note[1]%8]}_${note.slice(2).join('_')}`);
-                    break;
-                case "expanded_unmodified":
-                    if (note[1] < 8) {
-                        addToNotesList(timing, `${assignment[note[1]]}_${note.slice(2).join('_')}`);
-                    } else {
-                        addToNotesList(timing, note.slice(1).join('_'));
-                    }
-                    break;
+            break;
+          case "ignore":
+            // note modifiers are often index 3 (length 4)
+            if (note[1] < 8 && note.length == 3) {
+              addToNotesList(timing, `${assignment[note[1]]}_${note[2]}`);
             }
+            break;
+          case "expanded_truncate":
+            addToNotesList(
+              timing,
+              `${Math.floor(note[1] / 8) * 8 + assignment[note[1] % 8]}_${
+                note[2]
+              }`
+            );
+            break;
+          case "expanded_unmodified_truncate":
+            if (note[1] < 8) {
+              addToNotesList(timing, `${assignment[note[1]]}_${note[2]}`);
+            } else {
+              addToNotesList(timing, `${note[1]}_${note[2]}`);
+            }
+            break;
+          case "expanded":
+            addToNotesList(
+              timing,
+              `${Math.floor(note[1] / 8) * 8 + assignment[note[1] % 8]}_${note
+                .slice(2)
+                .join("_")}`
+            );
+            break;
+          case "expanded_unmodified":
+            if (note[1] < 8) {
+              addToNotesList(
+                timing,
+                `${assignment[note[1]]}_${note.slice(2).join("_")}`
+              );
+            } else {
+              addToNotesList(timing, note.slice(1).join("_"));
+            }
+            break;
         }
+      }
     }
 
     // we need to do these shenanigans due to the way sections can be formatted; sometimes all notes are grouped in one giant section
 
     lookupArray = Object.keys(notesList); // get note timings
 
-    lookupArray = lookupArray.concat(Object.keys(sectionList)) // concat with section timings
+    lookupArray = lookupArray.concat(Object.keys(sectionList)); // concat with section timings
 
     lookupArray = [...new Set(lookupArray)]; // remove duplicates
 
-    lookupArray.sort((a,b) => Number(a)-Number(b)) // sort
+    lookupArray.sort((a, b) => Number(a) - Number(b)); // sort
 
     for (var i = 0; i < lookupArray.length; i++) {
-        // if a valid section is at this lookup, add it to the scratch list
-        if (Object.keys(sectionList).includes(lookupArray[i])) {
-            scratchList.push(`#${sectionList[lookupArray[i]]}-${lookupArray[i]}`)
-        }
+      // if a valid section is at this lookup, add it to the scratch list
+      if (Object.keys(sectionList).includes(lookupArray[i])) {
+        scratchList.push(`#${sectionList[lookupArray[i]]}-${lookupArray[i]}`);
+      }
 
-        // if a valid note is at this lookup, add it to the scratch list
-        if (Object.keys(notesList).includes(lookupArray[i])) {
-            for (var x = 0; x < notesList[lookupArray[i]].length; x++) {
-                scratchList.push(`?${lookupArray[i]}_${notesList[lookupArray[i]][x]}`)
-            }
+      // if a valid note is at this lookup, add it to the scratch list
+      if (Object.keys(notesList).includes(lookupArray[i])) {
+        for (var x = 0; x < notesList[lookupArray[i]].length; x++) {
+          scratchList.push(
+            `?${lookupArray[i]}_${notesList[lookupArray[i]][x]}`
+          );
         }
+      }
     }
 
     // Create download link for outputted text file
-    const blob = new Blob([scratchList.join("\n")], {type : 'text/plain'});
-    const a = document.createElement('a');
+    const blob = new Blob([scratchList.join("\n")], { type: "text/plain" });
+    const a = document.createElement("a");
     document.body.appendChild(a);
     const url = window.URL.createObjectURL(blob);
-    const link = document.createElement('a');
+    const link = document.createElement("a");
 
     if (outputArea.style.display === "none") {
-        outputArea.style.display = "block"
+      outputArea.style.display = "block";
     }
 
     link.href = url;
-    link.innerText = 'Download';
+    link.innerText = "Download";
     link.download = file.name.split(".")[0] + ".txt";
-    link.id = "download";
-    link.addEventListener('click', function(){download()}, false);
+    link.classList.add("download");
+    link.addEventListener(
+      "click",
+      function () {
+        download();
+      },
+      false
+    );
     outputArea.appendChild(link);
-  }
+  };
 
   reader.onerror = (e) => {
     const error = e.target.error;
     console.error(`Error occured while reading ${file.name}`, error);
-  }
+  };
 
   reader.readAsText(file);
 }
